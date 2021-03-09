63d7b34e3e428ec74e53f533b4f8dee57cde4218
Katy Day <day.katy@gmail.com>
2021-03-09 16:28:39 +0000

    passed second test, lower treshold protected

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,4 +1,12 @@
 
 def band_pass_filter(track, lower, upper)
-  [40, 50, 60, 1000]
+  updated_track = []
+  track.each do |freq|
+    if freq < lower 
+      updated_track << lower
+    else
+      updated_track << freq
+    end
+  end
+   updated_track
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -2,9 +2,14 @@ require 'band_pass_filter'
 
 describe "#band_pass_filter" do
 
-describe "when given a track array within the filters, output is not changed" do
-  it "does not change" do 
+describe "when given a track array" do
+
+  it "does not change when within the filters" do 
     expect(band_pass_filter([40, 50, 60, 1000], 40, 1000)).to eq [40, 50, 60, 1000]
+  end
+
+  it "changes too-low frequencies to match the lowest filter" do
+    expect(band_pass_filter([30, 50,60, 1000], 40, 1000)).to eq [40, 50, 60, 1000]
   end
 end
 
